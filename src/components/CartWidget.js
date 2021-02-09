eeaecae80ee65a075aa012ec68d00b413be19170
Mike <miguel@dev.com>
2021-02-09 00:14:54 -0300

    -f CartWidget.js mostrar la cantidad de items agregados

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './CartWidget.css'
 import * as Icon from 'react-bootstrap-icons'
+import {CartContext} from "../CartContext";
 // import Cart from 'react-bootstrap-icons'
 
 const CartWidget = () => {
+	const cartContext = useContext(CartContext);
+
 	return (
 		<div>
 			<Icon.Cart3 className="product-cart" id="cart-icon" size="32"/>
-			<span className='badge badge-warning' id='cart-label-count'>1</span>
+			{
+				cartContext.products.length > 0 && (
+					<span className='badge badge-warning' id='cart-label-count'>{cartContext.getQuantity()}</span>
+				)
+			}
+
 		</div>
 	);
 };
