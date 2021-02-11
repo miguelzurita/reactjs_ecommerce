bb8dfb234fc662cfdf7f2c5944e82cae82a69273
Mike <miguel@dev.com>
2021-02-10 22:08:50 -0300

    + cargando los productos de firestore

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import ItemList from "./ItemList";
 import {useParams} from "react-router-dom";
-import {Container,Row} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 
 import {CartContext} from "../CartContext"
 import {getFireStore} from "../firebase";
@@ -14,24 +14,21 @@ const ItemListContainer = () => {
 
 	// const CartCont
 
-	let listProducts = [
+	/*let listProducts = [
 		{id: 1, id_category: 1, title: 'zapatilla', description: 'desc 1', stock: 2, price: 10, pictureUrl: 'https://via.placeholder.com/150/000000/FFFFFF/?text=zapatilla'},
 		{id: 2, id_category: 2, title: 'Remera', description: 'desc 2', stock: 3, price: 20, pictureUrl: 'https://via.placeholder.com/150/000000/FFFFFF/?text=remera'},
 		{id: 3, id_category: 3, title: 'Jean', description: 'desc 3', stock: 4, price: 30, pictureUrl: 'https://via.placeholder.com/150/000000/FFFFFF/?text=jean'},
 		{id: 4, id_category: 1, title: 'zapatilla 2', description: 'desc 1', stock: 2, price: 15, pictureUrl: 'https://via.placeholder.com/150/000000/FFFFFF/?text=zapatilla22'},
 		{id: 5, id_category: 2, title: 'Remera 2', description: 'desc 2', stock: 4, price: 25, pictureUrl: 'https://via.placeholder.com/150/000000/FFFFFF/?text=remera22'},
 		{id: 6, id_category: 3, title: 'Jean 2', description: 'desc 3', stock: 5, price: 35, pictureUrl: 'https://via.placeholder.com/150/000000/FFFFFF/?text=jean22'},
-	]
+	]*/
 
-	useEffect(()=>{
-		// setLoading(true);
-		let db = getFireStore()
-		const itemCollection = db.collection("products");
-		itemCollection.get().then()
+	useEffect(() => {
+
 	})
 
 
-	function simulateAPICall(listItems) {
+	/*function simulateAPICall(listItems) {
 		let promise = new Promise((resolve, reject) => {
 			let timeout = CALL_DELAY_MS;
 			setTimeout(function () {
@@ -44,16 +41,41 @@ const ItemListContainer = () => {
 		}, (error) => {
 			console.log("error")
 		})
-	}
+	}*/
 
 	//Al recibir el parametro
 	useEffect(() => {
-		console.log("idCategory:"+idCategory);
+		console.log("idCategory:" + idCategory);
+		// setLoading(true)
+		let db = getFireStore()
+		const itemCollection = db.collection("products");
+
+		let listProducts = itemCollection;
+
 		if (idCategory > 0) {
-			simulateAPICall(listProducts.filter(product => product.id_category.toString().includes(idCategory)));
-		}else{
-			simulateAPICall(listProducts);
+			// simulateAPICall(listProducts.filter(product => product.id_category.toString().includes(idCategory)));
+			listProducts = itemCollection.where('idCategory', '=', idCategory);
+		} else {
 		}
+
+		listProducts.get().then((querySnapshot) => {
+			if (querySnapshot.size === 0) {
+				console.log("no results");
+			} else {
+				console.log("docs:");
+				// console.log(querySnapshot.docs);
+				setProducts(querySnapshot.docs.map(doc => {
+					return {...doc.data(), id: doc.id}
+				}))
+				/*let tmp = querySnapshot.docs.map(doc => {
+					return {...doc.data(), id: doc.id}
+				})
+				console.log("tmo");
+				console.log(tmp);*/
+			}
+		}).catch((error) => {
+			console.log("error al recuperar los productos");
+		}).finally();
 
 	}, [idCategory])
 
