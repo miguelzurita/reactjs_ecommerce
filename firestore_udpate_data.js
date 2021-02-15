d9493d0af7770db352229674f45f4dc6c9dd6965
Mike <miguel@dev.com>
2021-02-15 17:15:13 -0300

    + importar productos y categorias en firestore

--- firestore_udpate_data.js
+++ firestore_udpate_data.js
@@ -1,19 +1,10 @@
 const firebase = require('firebase');
-// const firestore = require("firebase/firestore");
 require("firebase/firestore");
 
-let admin = require("firebase-admin");
-let serviceAccount = require("./reactjsecommerce-84729-firebase-adminsdk-y6yj2-7505982492.json");
+const csv = require('csv-parser');
+const fs = require('fs');
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
-});
-
-// console.log(admin.app.auth);
-// console.log(`admin.name:${admin}`);
-// var defaultAuth = admin.auth();
-// var defaultDatabase = admin.database();
-
+//Configurar firestore
 var firebaseConfig = {
 	apiKey: "AIzaSyBqYr9lECFmRZTDeiyUKlfOyZyDRZ-NGmk",
 	authDomain: "reactjsecommerce-84729.firebaseapp.com",
@@ -25,54 +16,25 @@ var firebaseConfig = {
 };
 
 const app = firebase.initializeApp(firebaseConfig)
-var db = firebase.firestore(app);
-const itemCollection = db.collection("products");
+let db = firebase.firestore(app);
 
-/*
-itemCollection.get().then((querySnapshot) => {
-	if (querySnapshot.size === 0) {
-		console.log("no results");
-	} else {
-		console.log(querySnapshot.docs);
-	}
-}).catch((error) => {
-	console.log("error al recuperar los productos");
-	console.log(error);
-}).finally();
-*/
-
-var productos = [{
-	"id": 1,
-	"category": "camas",
-	"title": "Moises WePets Classic Lunar",
-	"description": "",
-	"img": "244175.webp",
-	"size": [
-		"m"
-	],
-	"price": 3630,
-	"outstanding": "",
-	"discount": ""
-}];
-
-
-productos.forEach((obj) => {
-	db.collection("productos")
-		.add({
-			id: obj.id,
-			category: obj.category,
-			title: obj.title,
-			description: obj.description,
-			img: obj.img,
-			size: obj.size,
-			price: obj.price,
-			outstanding: obj.outstanding,
-			discount: obj.discount,
-		})
-		.then((docRef) => {
-			console.log("Producto registrado con ID: ", docRef.id);
-		})
-		.catch((error) => {
-			console.error("Error al agregar un documento: ", error);
-		});
-});
+//leer el csv e importarlo en firestore
+// fs.createReadStream('data_categories.csv')
+fs.createReadStream('data_products.csv')
+	.pipe(csv({separator: ';'}))
+	.on('data', (row) => {
+		// console.log(row);
+		// console.log(row.id + " "+row.name);
+		// db.collection("categories")
+		db.collection("products")
+			.add(row)
+			.then((docRef) => {
+				console.log("Producto registrado con ID: ", docRef.id);
+			})
+			.catch((error) => {
+				console.error("Error al agregar un documento: ", error);
+			});
+	})
+	.on('end', () => {
+		console.log('CSV file successfully processed');
+	});
