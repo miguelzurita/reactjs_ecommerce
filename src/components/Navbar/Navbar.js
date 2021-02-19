2fca49d4a8cf8b7ea1a6c6bf1e95bd9c14f59148
Mike <miguel@dev.com>
2021-02-19 00:32:10 -0300

    + Marcar la seccion actual

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,19 +15,19 @@ export const Navbar = () => {
 				FilVAR
 			</NavLink>
 			<div className="categories">
-				<NavLink to={`/`}>
+				<NavLink exact={true}  to={`/`} activeClassName='route-active'>
 					<p className="menu-item">Catalogo</p>
 				</NavLink>
-				<NavLink to={`/category/1`}>
+				<NavLink to={`/category/1`} activeClassName='route-active'>
 					<p className="menu-item">Aire</p>
 				</NavLink>
-				<NavLink to={`/category/2`}>
+				<NavLink to={`/category/2`} activeClassName='route-active'>
 					<p className="menu-item">Aceite</p>
 				</NavLink>
-				<NavLink to={`/category/3`}>
+				<NavLink to={`/category/3`} activeClassName='route-active'>
 					<p className="menu-item">Combustible</p>
 				</NavLink>
-				<NavLink to={`/category/4`}>
+				<NavLink to={`/category/4`} activeClassName='route-active'>
 					<p className="menu-item">Habitáculo</p>
 				</NavLink>
 				{
